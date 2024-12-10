20133997fbcb7e7f65ed8b7151915c89b045b7f9
haeun29 <hanh291029@naver.com>
2024-12-10 20:00:22 +0900

    C Basic

---
+++ main.cpp
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// int : %d
+// long long: %lld
+//  float: %f
+// double: %lf
+// char: %c
+
+
+int main() {
+    // int a = 1000;
+    // long long dientich = 1231231234141234;
+    // float dientich = 10.1234;
+    // double dientich = 124.1234123412;
+    // printf("%.10lf", dientich);
+    // char kitu = 'W';
+    // printf("%c", kitu);
+    // int a = 100, b = 200, c = 300;
+    // float d = 2.1231231;
+    // printf("%d \n", a);
+    // printf("%d ", b);
+    // printf("%d ", c);
+    // printf("gia tri cua ba bien la %d %d %d %.3f", a, b, c, d);
+
+    int a;
+    printf("nhap gia tri cho a ");
+    scanf("%d", &a);
+    printf("gia tri vua nhap %d", a);
+
+    return 0;
+}
